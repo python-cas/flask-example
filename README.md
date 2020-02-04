0a8a382066c159e22023684d859b34d1491099dd
Ming <ming@djangocas.dev>
2020-02-03 17:59:59 -0800

    Add link to README

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ python-cas Flask Example
 
 An example repo to demo how to integrate python-cas into a Flask app.
 
+## More
+
+- [python-cas Flask Example](https://djangocas.dev/blog/2020-02-03-python-cas-flask-example/)
+- [CAS 101](https://djangocas.dev/blog/2018-01-02-cas-101-introduction-to-cas-central-authentication-server/)
+
--- app.py
+++ app.py
@@ -1,56 +1,70 @@
 from flask import Flask, request, session, redirect, url_for
-from datetime import datetime
 from cas import CASClient
 
 app = Flask(__name__)
-
 app.secret_key = 'V7nlCN90LPHOTA9PGGyf'
 
 cas_client = CASClient(
     version=3,
-    service_url='http://localhost:5000/profile',
+    service_url='http://localhost:5000/login?next=%2Fprofile',
     server_url='https://django-cas-ng-demo-server.herokuapp.com/cas/'
-    )
+)
+
 
 @app.route('/')
 def index():
-    return redirect(url_for('profile'))
+    return redirect(url_for('login'))
+
 
 @app.route('/profile')
 def profile(method=['GET']):
     if 'username' in session:
-        return 'Already logged as %s. <a href="/logout">Logout</a>' % session['username']
+        return 'Logged in as %s. <a href="/logout">Logout</a>' % session['username']
+    return 'Login required. <a href="/login">Login</a>', 403
 
-    ticket = request.args.get('ticket')
-    if not ticket:
-        return 'User is not logged in. <a href="/login">Login</a>'
-
-    app.logger.debug('ticket: %s', ticket)
-
-    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
-
-    app.logger.debug('user: %s, attributes: %s, pgtiou: %s', user, attributes, pgtiou)
-
-    if not user:
-        return 'Failed to verify ticket. <a href="/login">Login</a>'
-    else:
-        session['username'] = user
-        return 'Logged in user: %s. <a href="/logout">Logout</a>' % user
 
 @app.route('/login')
 def login():
-    cas_login_url = cas_client.get_login_url()
-    app.logger.debug('cas_login_url: %s', cas_login_url)
-    return redirect(cas_login_url)
+    if 'username' in session:
+        # Already logged in
+        return redirect(url_for('profile'))
+
+    next = request.args.get('next')
+    ticket = request.args.get('ticket')
+    if not ticket:
+        # No ticket, the request come from end user, send to CAS login
+        cas_login_url = cas_client.get_login_url()
+        app.logger.debug('CAS login URL: %s', cas_login_url)
+        return redirect(cas_login_url)
+
+    # There is a ticket, the request come from CAS as callback.
+    # need call `verify_ticket()` to validate ticket and get user profile.
+    app.logger.debug('ticket: %s', ticket)
+    app.logger.debug('next: %s', next)
+
+    user, attributes, pgtiou = cas_client.verify_ticket(ticket)
+
+    app.logger.debug(
+        'CAS verify ticket response: user: %s, attributes: %s, pgtiou: %s', user, attributes, pgtiou)
+
+    if not user:
+        return 'Failed to verify ticket. <a href="/login">Login</a>'
+    else:  # Login successfully, redirect according `next` query parameter.
+        session['username'] = user
+        return redirect(next)
+
 
 @app.route('/logout')
 def logout():
-    cas_logout_url = cas_client.get_logout_url(url_for('logout_callback', _external=True))
-    app.logger.debug('cas_logout_url: %s', cas_logout_url)
+    redirect_url = url_for('logout_callback', _external=True)
+    cas_logout_url = cas_client.get_logout_url(redirect_url)
+    app.logger.debug('CAS logout URL: %s', cas_logout_url)
 
     return redirect(cas_logout_url)
 
+
 @app.route('/logout_callback')
 def logout_callback():
+    # redirect from CAS logout request after CAS logout successfully
     session.pop('username', None)
     return 'Logged out from CAS. <a href="/login">Login</a>'
